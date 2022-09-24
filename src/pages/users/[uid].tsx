749667bb69663ba94c5b4399494e636d215998d2
unreact-hatano <mizutama.hahaha@gmail.com>
2022-09-25 04:55:21 +0900

    フォーム作成

--- src/pages/users/[uid].tsx
+++ src/pages/users/[uid].tsx
@@ -1,8 +1,17 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { User } from "../../models/User";
-import { collection, doc, getDoc, getFirestore } from "firebase/firestore";
-
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getFirestore,
+  serverTimestamp,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import Layout from "../../components/Layout";
+import { useAuthentication } from "../../hooks/authentication";
 type Query = {
   uid: string;
 };
@@ -11,6 +20,36 @@ export default function UserShow() {
   const [user, setUser] = useState<User>(null);
   const router = useRouter();
   const query = router.query as Query;
+  const { user: currentUser } = useAuthentication();
+  const [body, setBody] = useState("");
+  const [isSending, setIsSending] = useState(false);
+
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const db = getFirestore();
+    setIsSending(true);
+
+    await addDoc(collection(db, "question"), {
+      senderUid: currentUser.uid,
+      receiverUid: user.uid,
+      body,
+      isReplied: false,
+      createdAt: serverTimestamp(),
+    });
+
+    setIsSending(false);
+    setBody("");
+    toast.success("質問を送信しました。", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
 
   useEffect(() => {
     if (query.uid === undefined) {
@@ -33,5 +72,43 @@ export default function UserShow() {
     loadUser();
   }, [query.uid]);
 
-  return <div>{user ? user.name : "ロード中..."}</div>;
+  return (
+    <>
+      <Layout>
+        {user && (
+          <div className="text-center">
+            <h1 className="h4">{user.name}さんのページ</h1>
+            <div className="m-5">{user.name}さんに質問しよう！</div>
+          </div>
+        )}
+      </Layout>
+      <div className="row justify-content-center mb-3">
+        <div className="col-12 col-md-6">
+          <form onSubmit={onSubmit}>
+            <textarea
+              className="form-control"
+              placeholder="お元気ですか"
+              rows={6}
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+              required
+            ></textarea>
+            <div className="m-3">
+              {isSending ? (
+                <div className="spinner-border text-secondary" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              ) : (
+                <button type="submit" className="btn btn-primary">
+                  質問を送信する
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
+
+// visually-hidden:bootstrapのバージョンと噛み合っておらず上手く動かない
